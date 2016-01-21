42ec009263a281a6a494fc490ce0695b6cf3d03b
Sebastian Mobo <stmobo@users.noreply.github.com>
2016-01-21 12:55:43 -0600

    Add basic trajectory data finding functions

--- trajectory.h
+++ trajectory.h
@@ -1,9 +1,16 @@
+#pragma once
+
 #include <vector>
 #include <tuple>
+#include <algorithm>
+#include "Integrator.h"
 
 class trajectory {
   std::vector< std::pair<double, PhysState> > points;
   
   points.iterator iterator() { return points.begin(); }
   void addPoint(double t, PhysState st) { points.push_back( std::make_pair(t, st) ); }
+  std::pair<double, PhysState> findMaxHeight();
+  double getDistance() { return points.back()[1].pos.x; }
+  double getFlightTime() { return points.back()[0]; }
 };
