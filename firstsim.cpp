7907ab95b40556a2cb0e2631dd28cb49d6ff775c
Sebastian Mobo <stmobo@users.noreply.github.com>
2016-01-21 12:44:16 -0600

    Start work on FRC 2016 simulator

---
+++ firstsim.cpp
@@ -0,0 +1,40 @@
+namespace frc2016 {
+
+const double boulder_diameter = .254; // m
+const double boulder_mass = .294835; // kg
+const double boulder_drag_coeff = 0.47;
+const double wheel_mass = 0; // TODO: fill this in
+const double boulder_iner_moment = 0;
+const double wheel_iner_moment = 0;
+const double timestep = 0.01;
+
+const std::vector<AccelerateFunction*> forceComp = {
+  new spinLiftAcc(boulder_diameter / 2, boulder_mass),
+  new dragAcc(boulder_diameter, boulder_mass, boulder_drag_coeff),
+  new gravAcc() }; 
+
+PhysState frc2016_initalize(double avel1, double avel2, double initElev) {
+  double ballAngVel = (wheel_iner_moment * (avel1+avel2)) / boulder_iner_moment;
+  double ballLinVel = ( (wheel_iner_moment * ((avel1+avel2) * (avel1+avel2))) - (boulder_iner_moment * ballAngVel * ballAngVel) ) / boulder_mass;
+  
+  PhysState out;
+  out.angVel = ballAngVel;
+  out.linVel.x = ballLinVel;
+  out.linVel.y = 0;
+  out.pos.y = initElev;
+  
+  return out;
+}
+
+void frc2016_simulate(PhysState initState) {
+  PhysState cur = initState;
+  double curTime = 0;
+  
+  while(cur.pos.y >= 0) {
+    cur = integrate(cur, curTime, timestep, forceComp);
+    curTime += timestep;
+  }
+  
+}
+
+}
