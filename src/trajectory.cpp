7d08f38224dfb6807208c21c85a6e445e4c16a27
Sebastian Mobo <stmobo@gmail.com>
2016-01-21 17:20:20 -0600

    Fix trajectory point references

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -5,7 +5,7 @@
 #include "trajectory.h"
 #include "Integrator.h"
 
-static bool cmpTrajPtHeight( trajPt p1, trajPt p2) { return (p1[1].pos.y < p2[1].pos.y); };
+static bool cmpTrajPtHeight( trajPt p1, trajPt p2) { return (p1.first.pos.y < p2.second.pos.y); };
 
 trajPt trajectory::findMaxHeight() {
   vector<trajPt>::iterator maxElem = std::max_element(points.begin(), points.end(), cmpTrajPtHeight);
