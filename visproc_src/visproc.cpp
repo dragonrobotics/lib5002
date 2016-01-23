dea7589432d1a7975e9cd9ae5c5d989d541c8df7
Sebastian Mobo <stmobo@gmail.com>
2016-01-23 01:30:07 -0600

    Added boulder processing code

--- visproc_src/visproc.cpp
+++ visproc_src/visproc.cpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <iostream>
 
+/* Thresholds for goal detection: */
 /* hue threshold: min/max; target seems to be 89 for the test image. */
 /* 75-100 (150 - 200 with 360 degree Hue) */
 const unsigned char hueThres[2] = {85, 95};
@@ -14,16 +15,18 @@ const unsigned char hueThres[2] = {85, 95};
 /* though of course you need to make it 0-255 instead of 0-100 */
 /* 45 - 75 works? though it's 115 - 191 in OpenCV */
 const unsigned char valThres[2] = {115, 191};
+
 const double cannyThresMin = 10;
 const double cannyThresSize = 10; /* Max = ThresMin + ThresSize */
+const double pi = 3.14159265358979323846;
 
 void convert_and_write(const char* filename, cv::Mat hsvimg) {
-    cv::Mat tmp;
-    cv::cvtColor(hsvimg, tmp, CV_HSV2BGR);
-    cv::imwrite(filename, tmp);
+        cv::Mat tmp;
+        cv::cvtColor(hsvimg, tmp, CV_HSV2BGR);
+        cv::imwrite(filename, tmp);
 }
 
-cv::Mat preprocess_pipeline(cv::Mat input) {
+cv::Mat goal_preprocess_pipeline(cv::Mat input) {
         cv::Mat tmp(input.size(), input.type());
 
         cv::cvtColor(input, tmp, CV_BGR2HSV);
@@ -45,10 +48,40 @@ cv::Mat preprocess_pipeline(cv::Mat input) {
         /* Blur for edge detection */
         cv::Mat edgedet;
         cv::blur(mask, edgedet, cv::Size(3,3));
-        cv::imwrite("pipeline_stage4.png", mask);
+        cv::imwrite("pipeline_stage4.png", edgedet);
 
         cv::Canny(edgedet, edgedet, cannyThresMin, cannyThresMin+cannyThresSize);
-        cv::imwrite("pipeline_stage5.png", mask);
+        cv::imwrite("pipeline_stage5.png", edgedet);
+
+        return edgedet;
+}
+
+cv::Mat boulder_preprocess_pipeline(cv::Mat input) {
+        cv::Mat tmp(input.size(), input.type());
+
+        cv::cvtColor(input, tmp, CV_BGR2HSV);
+
+        /* Make things easier for the HV filter */
+        //cv::blur(tmp, tmp, cv::Size(5,5));
+        cv::GaussianBlur(tmp, tmp, cv::Size(7,7), 3.5, 3.5, cv::BORDER_REPLICATE);
+        convert_and_write("pipeline_stage1.png", tmp);
+
+        /* Filter on saturation and brightness */
+        cv::Mat mask(input.size(), CV_8U);
+        cv::inRange(tmp, cv::Scalar(0,0,102), cv::Scalar(255,26,191), mask);
+        cv::imwrite("pipeline_stage2.png", mask);
+
+        /* Erode away smaller hits */
+        cv::erode(mask, mask, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7,7)));
+        cv::imwrite("pipeline_stage3.png", mask);
+
+        /* Blur for edge detection */
+        cv::Mat edgedet;
+        cv::blur(mask, edgedet, cv::Size(5,5));
+        cv::imwrite("pipeline_stage4.png", edgedet);
+
+        cv::Canny(edgedet, edgedet, cannyThresMin, cannyThresMin+cannyThresSize);
+        cv::imwrite("pipeline_stage5.png", edgedet);
 
         return edgedet;
 }
@@ -60,8 +93,8 @@ bool scoresort(scoredContour c1, scoredContour c2) {
 }
 
 double scoreDistanceFromTarget(const double target, double value) {
-    double distanceRatio = (fabs(target - fabs(target - value)) / target);
-    return fmax(0, fmin(distanceRatio*100, 100));
+        double distanceRatio = (fabs(target - fabs(target - value)) / target);
+        return fmax(0, fmin(distanceRatio*100, 100));
 }
 
 scoredContour goal_pipeline(cv::Mat input) {
@@ -83,11 +116,21 @@ scoredContour goal_pipeline(cv::Mat input) {
 
                 const double cvarea_target = (80.0/240.0);
                 const double asratio_target = (20.0/12.0);
+                const double area_threshold = 1000;
 
                 std::cout << std::endl;
                 std::cout << "Contour " << ctr << ": " << std::endl;
+                ctr++;
                 std::cout << "Area: "  << area << std::endl;
                 std::cout << "Perimeter: " << perimeter << std::endl;
+
+                /* Area Thresholding Test
+                 * Only accept contours of a certain total size.
+                 */
+
+                if(area < area_threshold) {
+                    continue;
+                }
 
                 /* Coverage Area Test
                  * Compare particle area vs. Bounding Rectangle area.
@@ -106,18 +149,34 @@ scoredContour goal_pipeline(cv::Mat input) {
                 double aspect_ratio = tmp / bounds.height;
                 double ar_score = scoreDistanceFromTarget(asratio_target, aspect_ratio);
 
+                /* Image Moment Test
+                 * Computes image moments and compares it to known values.
+                 */
+
+                cv::Moments m = cv::moments(*i);
+                double moment_score = scoreDistanceFromTarget(0.28, m.nu02);
+
+                /* Image Orientation Test
+                 * Computes angles off-axis or contours.
+                 */
+                // theta = (1/2)atan2(mu11, mu20-mu02) radians
+                // theta ranges from -90 degrees to +90 degrees.
+                double theta = (atan2(m.mu11,m.mu20-m.mu02) * 90) / pi;
+                double angle_score = (90 - fabs(theta))+10;
+
+                std::cout << "nu-02: " << m.nu02 << std::endl;
                 std::cout << "CVArea: "  <<  coverage_area << std::endl;
                 std::cout << "AsRatio: " << aspect_ratio << std::endl;
+                std::cout << "Orientation: " << theta << std::endl;
 
-                double total_score = (cvarea_score + ar_score) / 2;
-
-                total_score += area; // prioritize larger blobs
+                double total_score = (moment_score + cvarea_score + ar_score + angle_score) / 4;
 
                 std::cout << "CVArea Score: "  <<  cvarea_score << std::endl;
                 std::cout << "AsRatio Score: " << ar_score << std::endl;
+                std::cout << "Moment Score: " << moment_score << std::endl;
+                std::cout << "Angle Score: " << angle_score << std::endl;
                 std::cout << "Total Score: " << total_score << std::endl;
 
-                ctr++;
                 finalscores.push_back(std::make_pair(total_score, std::move(*i)));
         }
 
@@ -126,12 +185,78 @@ scoredContour goal_pipeline(cv::Mat input) {
         return finalscores.back();
 }
 
+scoredContour boulder_pipeline(cv::Mat input) {
+    std::vector< std::vector<cv::Point> > contours;
+
+    cv::Mat contourOut = input.clone();
+
+    cv::findContours(contourOut, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    std::vector<scoredContour> finalscores;
+
+    std::cout << "Found " << contours.size() << " contours." << std::endl;
+
+    unsigned int ctr = 0;
+    for(std::vector< std::vector<cv::Point> >::iterator i = contours.begin();
+        i != contours.end(); ++i) {
+            ctr++;
+            double area = cv::contourArea(*i);
+            double perimeter = cv::arcLength(*i, true);
+            cv::Rect bounds = cv::boundingRect(*i);
+
+            const double area_threshold = 5000;
+
+            if(area < area_threshold) {
+                continue;
+            }
+
+            std::cout << std::endl;
+            std::cout << "Contour " << ctr << ": " << std::endl;
+            ctr++;
+            std::cout << "Area: "  << area << std::endl;
+            std::cout << "Perimeter: " << perimeter << std::endl;
+
+            double idealRadius = (bounds.width/2);
+            double idealArea = pi * (idealRadius * idealRadius);
+
+            double circularity = scoreDistanceFromTarget(idealArea, area);
+            double ar_score = scoreDistanceFromTarget(1, bounds.width / bounds.height);
+
+            double total_score = (circularity + ar_score) / 2;
+
+            std::cout << "Circularity Score: " << circularity << std::endl;
+            std::cout << "AsRatio Score: " << ar_score << std::endl;
+            std::cout << "Total Score: " << total_score << std::endl;
+
+            finalscores.push_back(std::make_pair(total_score, std::move(*i)));
+    }
+
+    std::sort(finalscores.begin(), finalscores.end(), &scoresort);
+
+    return finalscores.back();
+}
+
 int main(int argc, char** argv) {
+        if(argc == 1) {
+            std::cout << "Need input file to process." << std::endl;
+            return 1;
+        }
         std::string infile = argv[1];
+        bool boulderproc = false;
+        if(argc > 2) {
+            std::string opt2 = argv[2];
+            if(opt2 == "--boulder") {
+                boulderproc = true;
+            }
+        }
 
         cv::Mat src = cv::imread(infile, 1);
 
-        scoredContour out = goal_pipeline(preprocess_pipeline(src));
+        scoredContour out;
+        if(!boulderproc) {
+            out = goal_pipeline(goal_preprocess_pipeline(src));
+        } else {
+            out = boulder_pipeline(boulder_preprocess_pipeline(src));
+        }
 
         cv::Mat output = cv::Mat::zeros(src.size(), CV_8UC3);
 
