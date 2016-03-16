571a4506d0ef887e65f2e51582e7c16040424604
Sebastian Mobo <stmobo@gmail.com>
2016-03-16 17:37:14 -0500

    Add fully functional discover server testing

--- Makefile
+++ Makefile
@@ -5,15 +5,19 @@ include ./net_src/Makefile
 $(NET_OBJ_OUT_PATH)server.o: ./server2016_src/server.cpp
 	$(CXX) --std=c++14 -c -o $@ $(NET_INC_FLAGS) $(VIS_INC_FLAGS) $<
 
+$(NET_OBJ_OUT_PATH)discover_server.o: ./server2016_src/discserver.cpp
+	$(CXX) --std=c++14 -c -o $@ $(NET_INC_FLAGS) $<
+
 $(OUTDIR)/server2016: $(NET_OBJ_OUT_PATH)server.o $(OUTDIR)/lib5002-vis.so $(OUTDIR)/lib5002-net.so $(OUTDIR)/lib5002-stream.so
 	$(CXX) -o $(OUTDIR)/server2016 $< -L$(OUTDIR) -l5002-stream -l5002-net -l5002-vis $(NET_LIB_FLAGS) $(VIS_LIB_FLAGS) -pthread -ldl
 
+$(OUTDIR)/5002-discover: $(NET_OBJ_OUT_PATH)discover_server.o $(OUTDIR)/lib5002-net.so
+	$(CXX) -o $(OUTDIR)/discover_server $< -L$(OUTDIR) -l5002-net $(NET_LIB_FLAGS) -pthread -ldl
+
 server2016: $(OUTDIR)/server2016
+5002-discover: $(OUTDIR)/5002-discover
 
-PROGRAMS += server2016
-
-
-
+PROGRAMS += server2016 5002-discover
 
 outdirs:
 	$(foreach dir, $(OUTDIRS), mkdir -p $(dir) ;)
---
+++ server2016_src/discserver.cpp
@@ -0,0 +1,44 @@
+#include "sockwrap.h"
+#include "netmsg.h"
+#include "msgtype.h"
+#include "network_bytestream.h"
+
+const int serverPort = 5800;
+
+int main() {
+	serverSocket sock(serverPort, SOCK_DGRAM);
+	std::cout << "Listening on " << (std::string)sock.getbindaddr() << std::endl;	
+	while(true) {
+		netmsg msg = sock.recv(0);
+	
+		if(message::is_valid_message(static_cast<void*>(msg.getbuf().get()))) {
+			std::shared_ptr<message> msgdata(reinterpret_cast<message*>(msg.getbuf().get()));
+			if(msgdata->type == message_type::DISCOVER) {
+				std::cout << "Received DISCOVER message from " << (std::string)msg.addr;
+				discover_msg retm(origin_t::JETSON);
+				netmsg out = message::wrap_packet(&retm);
+				out.addr = msg.addr;
+
+				sock.send(out);
+
+				std::unique_ptr<message_payload> recvm = msgdata->unwrap_packet();
+				discover_msg* payload = static_cast<discover_msg*>(recvm.get());
+
+				switch(payload->origin) {
+				case origin_t::DRIVER_STATION:
+					std::cout << ", a driver station." << std::endl;
+					break;
+				case origin_t::ROBORIO:
+					std::cout << ", a RoboRio!" << std::endl;
+					break;
+				case origin_t::JETSON:
+					std::cout << ", another Jetson?" << std::endl;
+					break;
+				case origin_t::UNKNOWN:
+					std::cout << ", an unknown type of sender!!" << std::endl;
+					break;			
+				}		
+			}
+		}
+	}
+}
